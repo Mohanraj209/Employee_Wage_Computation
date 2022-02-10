970c68611e69cb508c024acee4b9da10743818ae
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-11 03:36:49 +0530

    Program to Calculate Wages for a Month

---
+++ EmpWageComputation/UC5_WageMonth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    internal class UC5_WageMonth
+    {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public static void CalculateWagesFor20DaysInMonth()
+        {
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 16;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;   //int a=6; a+=10;a=a+10
+                Console.WriteLine("Emp Wage: " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+        }
+    }
+}
