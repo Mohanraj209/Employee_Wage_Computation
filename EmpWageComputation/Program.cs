2222ab8b3f5452e78004c2a1613bb7427d141551
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-11 02:34:44 +0530

    Displaying Welcome to Employee Wage Computaton Program

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------Welcome to Employee Wage Computation Program---------");
+        }
+
+    }
+
+}
