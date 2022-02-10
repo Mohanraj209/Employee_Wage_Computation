f8ace2ce93a109940bbe512cfa819a09d25d6843
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-11 03:31:17 +0530

    Program to Add Part Time Employee Wage using Switch Case

---
+++ EmpWageComputation/UC4_SwitchCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    internal class UC4_SwitchCase
+    {
+        public const int IS_PART_TIME = 1;
+        //public readonly int Test=12;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public static void EmpWageUsingSwitch()
+        {
+            //Constants
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 16;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
+        }
+
+    }
+
+}
